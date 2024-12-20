efb62351c28fca21ffa2dd085206d245fc4197c8
Barco <44333353+Niki106@users.noreply.github.com>
2024-12-20 10:21:57 +0100

    Update database.py

--- app/dependencies/database.py
+++ app/dependencies/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqldb://root:glowglow@localhost/test"
+# DATABASE_URL = "mysql+mysqldb://root:@localhost/test"
+# DATABASE_URL = "postgresql://postgres:glowglow@localhost:5432/salesorder"
+DATABASE_URL = "postgresql://doadmin:AVNS_OPaHDSDw7xOgi5H-yfl@db-postgresql-nyc3-28362-do-user-18300103-0.i.db.ondigitalocean.com:25060/defaultdb"
 
 db_engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=db_engine)
